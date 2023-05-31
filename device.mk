974810df226a29b6fb63cd4ace2a2a53d70ace5d
Tim Zimmermann <tim@linux4.de>
2023-05-31 17:32:56 +0200

    rosemary: Switch to new AIDL memtrack HAL
    Change-Id: I3ae077b5f59e4fcf32489eef64bce8551d758998
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -148,9 +148,10 @@ PRODUCT_PACKAGES += \
     libdrm.vendor \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.composer@2.1.vendor \
-    android.hardware.graphics.composer@2.1-resources.vendor \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.graphics.composer@2.1-resources.vendor
+
+PRODUCT_PACKAGES += \
+    android.hardware.memtrack-service.mediatek-mali
 
 PRODUCT_PACKAGES += \
     libfmq \
