becf82b6f410c1a63e9371f50342fa10c60c9079
bengris32 <bengris32@protonmail.ch>
2023-12-19 15:25:28 +0000

    rosemary: Move to common sensors service
    Change-Id: Idebada879d5c64d59c2bc2ee06146f33b862538d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -476,7 +476,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
